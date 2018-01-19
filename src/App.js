b427e6a76ad509a66e5a7fe41e1ad965c9c0cd5f
Eduardo Fernandez <eduardo.fernandez@mediasq.com>
2018-01-19 16:48:04 +0100

    Set props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 
 class App extends React.Component {
   render() {
-    return <h1>Hello</h1> 
+    let txt = this.props.txt;
+    let cat = this.props.cat;
+    return (
+      <div>
+        <h1>Hallo</h1>
+        <b>{txt}</b> <i>Category: {cat}</i>
+      </div>
+    )
   }
+}
+
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "I go through all this before you wake up",
+  cat: 6
 }
 
 export default App;
