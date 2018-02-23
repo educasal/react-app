ea11701a3c22fa551868a0391351f01c10948d6b
Edu Casal <eduardo.fernandez.casal@gmail.com>
2018-02-24 00:28:29 +0100

    Validate text field

--- src/NestedDataApp.js
+++ src/NestedDataApp.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 class NestedDataApp extends React.Component {
 
   render() {
-    return <Button>I <Heart /> React</Button>
+    return (
+      <div>
+        <Title text="Title" />
+        <Button>I <Heart /> React</Button>
+      </div>
+    )
   }
 }
 
 const Button = (props) => <button>{props.children}</button>
 const Heart = () => <span>&hearts;</span>
+const Title = (props) => <h1>{props.text}</h1>
+
+Title.propTypes = {
+  text(props, propName, component) {
+    if (!(propName in props)) {
+      return new Error(`Missing ${propName}`)
+    }
+    if (props[propName].length < 6) {
+      return new Error(`${propName} is too short`)
+    }
+  }
+}
 
 export default NestedDataApp;
