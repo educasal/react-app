813f0719d33f41f50e7c9f682e3877e523d1ee35
Edu Casal <eduardo.fernandez.casal@gmail.com>
2018-02-26 23:51:53 +0100

    Reference specific components

--- src/EventsApp.js
+++ src/EventsApp.js
@@ -4,16 +4,26 @@ class EventsApp extends React.Component {
 
   constructor(){
     super();
-    this.state = {currentEvent: '---'}
+    this.state = { currentEvent: '---',
+                   a: 'a',
+                   b: 'b',
+                   c: 'c'
+                 }
     this.update = this.update.bind(this)
   }
 
   update(e){
-    this.setState({currentEvent: e.type})
+    this.setState({ currentEvent: e.type,
+                    a: this.refs.a.value,
+                    b: this.refs.b.value,
+                    c: this.c.refs.input.value
+                  })
   }
+
   render(){
    return (
      <div>
+       <h1>{this.state.currentEvent}</h1>
        <textarea
          onKeyPress={this.update}
          onCopy={this.update}
@@ -27,9 +37,25 @@ class EventsApp extends React.Component {
          onTouchEnd={this.update}
          cols="30"
          rows="10" />
-       <h1>{this.state.currentEvent}</h1>
+      <hr />
+      <h1>{this.state.a}</h1>
+      <input ref="a" type="text" onChange={this.update} />
+      <hr />
+      <h1>{this.state.b}</h1>
+      <input ref="b" type="text" onChange={this.update} />
+      <hr />
+      <h1>{this.state.c}</h1>
+      <Input update ={this.update.bind(this)} ref={component => this.c = component} />
+
      </div>
   )
  }
 }
+
+class Input extends React.Component {
+  render() {
+    return <div><input type="text" onChange={this.props.update} ref="input" /></div>
+  }
+}
+
 export default EventsApp
